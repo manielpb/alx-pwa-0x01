8ee85026272226c0f25bf546a22db7a133b8a779
maniel <manuelpepr99@icloud.com>
2025-10-26 02:27:34 +0000

    Link the Manifest in _document.js

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,9 +1,27 @@
-import { Html, Head, Main, NextScript } from "next/document";
+/*import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+*/
+import { Html, Head, Main, NextScript } from "next/document";
+
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
